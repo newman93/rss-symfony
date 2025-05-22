c98be7736c2f3cb05c56c20348e48b6118fd92c5
Adrian Kowalski <kowalski.adrian.5@gmail.com>
2025-05-22 21:41:29 +0200

    Basic controllers

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response {
+        $user = $this->getUser();
+
+        if ($user) {
+
+        }
+
+        return $this->render('home/index.html.twig');
+    }
+}
