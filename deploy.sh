47eb4e8c2af54c505fb47f4aab2ace3904557469
Adrian Kowalski <kowalski.adrian.5@gmail.com>
2025-05-23 08:50:40 +0200

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ cd "$1"
 # Update kodu
 git pull origin main
 # Restart kontenera, aby poszła aktualizacja
-docker container restart symfony_rss_apache
+docker container stop symfony_rss_apache
+docker container start symfony_rss_apache
