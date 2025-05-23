d0dd4568772b1a29f034d1f31a664f5254a1cf41
Adrian Kowalski <kowalski.adrian.5@gmail.com>
2025-05-23 08:24:56 +0200

    Simple deploy

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,5 @@ set -e
 cd "$1"
 # Update kodu
 git pull origin main
-# Instalacje backend
-docker exec symfony_rss_apache bash -c "composer install --no-interaction --prefer-dist --optimize-autoloader"
-# Migruj bazę 
-docker exec symfony_rss_apache bash -c "php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration"
-# Frontend build 
-docker exec symfony_rss_apache bash -c "npm install"
-docker exec symfony_rss_apache bash -c "npm run build"
+# Restart kontenera, aby poszła aktualizacja
+docker container restart symfony_rss_apache
